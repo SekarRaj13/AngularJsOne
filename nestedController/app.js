7fd60e646d0739d6b7c19d460580f9b5019d07fd
sekarraj <sekarraj205@gmail.com>
2018-02-16 23:05:56 +0530

    controller name used with "as" keyword

--- nestedController/app.js
+++ nestedController/app.js
@@ -2,11 +2,11 @@ var app = angular.module("nested", []);
 
 app.controller("nestedController1", function ($scope) {
 	// body...
-	$scope.controller1 = "controller1";
+	this.controller1 = "controller1";
 
 })
 
 app.controller("nestedController2",function ($scope) {
 	// body...
-	// $scope.controller1 = "controller2";
+	 this.controller1 = "controller2";
 })
