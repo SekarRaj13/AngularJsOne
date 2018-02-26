0500cb089986f4d81463bca0634a9c4e37636b5c
sekarraj <sekarraj205@gmail.com>
2018-02-26 21:03:13 +0530

    ng-repeat with parameters like $index, $first, $even, $odd, $last

--- moreDirectivesApp/app.js
+++ moreDirectivesApp/app.js
@@ -2,5 +2,17 @@ var app = angular.module("moreDirApp", []);
 
 app.controller("moreDirCtrl", function () {
 
+	this.myList = [{"name":"sekar",
+					"age":26		
+				   },
+				   {"name":"raj",
+					"age":22		
+				   },
+				   {"name":"taya",
+					"age":21		
+				   },
+				   {"name":"yaya",
+					"age":29		
+				   },];
 
 })
